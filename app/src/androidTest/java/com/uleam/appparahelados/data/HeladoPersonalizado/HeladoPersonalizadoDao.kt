032eee2f91fe4f3ccff990810b8e949e24af3128
Jonathan Marin <44934923+iTzJonathanxD@users.noreply.github.com>
2024-05-17 16:45:15 -0500

    base de datos

--- app/src/androidTest/java/com/uleam/appparahelados/data/HeladoPersonalizado/HeladoPersonalizadoDao.kt
+++ app/src/androidTest/java/com/uleam/appparahelados/data/HeladoPersonalizado/HeladoPersonalizadoDao.kt
@@ -1,4 +1,18 @@
 package com.uleam.appparahelados.data.HeladoPersonalizado
 
-class HeladoPersonalizadoDao {
+import androidx.room.*
+
+@Dao
+interface HeladoPersonalizadoDao {
+    @Insert
+    suspend fun insert(heladoPersonalizado: HeladoPersonalizado): Long
+
+    @Query("SELECT * FROM helados_personalizados")
+    suspend fun getAllHeladosPersonalizados(): List<HeladoPersonalizado>
+
+    @Update
+    suspend fun updateHeladoPersonalizado(heladoPersonalizado: HeladoPersonalizado)
+
+    @Delete
+    suspend fun deleteHeladoPersonalizado(heladoPersonalizado: HeladoPersonalizado)
 }
--- app/src/androidTest/java/com/uleam/appparahelados/data/HeladoTopping/HeladoToppingDao.kt
+++ app/src/androidTest/java/com/uleam/appparahelados/data/HeladoTopping/HeladoToppingDao.kt
@@ -1,4 +1,15 @@
 package com.uleam.appparahelados.data.HeladoTopping
 
-class HeladoToppingDao {
+import androidx.room.*
+
+@Dao
+interface HeladoToppingDao {
+    @Insert
+    suspend fun insert(heladoTopping: HeladoTopping)
+
+    @Query("SELECT * FROM helado_toppings")
+    suspend fun getAllHeladoToppings(): List<HeladoTopping>
+
+    @Delete
+    suspend fun deleteHeladoTopping(heladoTopping: HeladoTopping)
 }
