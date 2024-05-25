9eb4947aa755a6e1b6106731ddb1d682df52a17e
fabricio zavala <fabriciozavala13@gmail.com>
2024-05-25 01:15:33 -0500

    mejora...

--- app/src/main/java/com/uleam/appparahelados/ui/login/SplashScreen.kt
+++ app/src/main/java/com/uleam/appparahelados/ui/login/SplashScreen.kt
@@ -1,16 +1,19 @@
 package com.uleam.appparahelados.ui.splash
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.uleam.appparahelados.R
@@ -25,19 +28,44 @@ fun SplashScreen(navController: NavController) {
         }
     }
 
-    Column(
+    Box(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        contentAlignment = Alignment.Center
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.portada),
-            contentDescription = "Portada"
-        )
-        Text(
-            text = "Heladería Sammy",
-            color = Color.Black,
-            fontSize = 32.sp
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.portada),
+                contentDescription = "Portada",
+                modifier = Modifier
+                    .size(220.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Gray, CircleShape)
+            )
+            Text(
+                text = "Heladería Sammy",
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 42.sp
+            )
+            Text(
+                text = "¡Bienvenido!",
+                color = Color.Gray,
+                fontSize = 26.sp
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 16.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(
+                text = "by jony y fabricio",
+                color = Color.Blue,
+                fontSize = 25.sp
+            )
+        }
     }
 }
