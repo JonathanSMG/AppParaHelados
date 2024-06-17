6c3e1b753b8c2c87bb9d734405e30cc35c704d98
n3bazort <n3bazort@gmail.com>
2024-06-16 19:56:23 -0500

    Creacion de registroScreen

--- flutter_app/lib/ui/adminHome/AdminScreen.dart
+++ flutter_app/lib/ui/adminHome/AdminScreen.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
       home: AdminPage(),
     );
   }
@@ -19,101 +22,46 @@ class AdminPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: Text('Administrador'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.red,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage('assets/images/profile.png'),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Welcome,',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                  Text(
-                    'ZAVALA REYES',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.inbox),
-              title: Text('Inbox'),
-              onTap: () {},
-            ),
-          ],
-        ),
+        title: const Text('Administrador'),
+          centerTitle: true,
+
+
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+               const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Text(
-                    'SABORES',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red,
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Welcome,', style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 16)),
+                      Text('ZAVALA REYES', style: TextStyle(color: Color.fromARGB(255, 2, 0, 0), fontSize: 20, fontWeight: FontWeight.bold)),
+                    ],
                   ),
-                  IconButton(
-                    icon: Icon(Icons.add_circle, color: Colors.yellow),
-                    onPressed: () {},
+                  SizedBox(width: 10),
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('../assets/images/fotoperfil.jpg'),
                   ),
                 ],
               ),
-            ),
-            _buildItemList('Chocolate', 'assets/images/chocolate.jpg'),
-            _buildItemList('Vainilla', 'assets/images/vainilla.jpg'),
-            _buildItemList('Fresa', 'assets/images/fresa.jpg'),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'TOPPINGS',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red,
-                    ),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.add_circle, color: Colors.yellow),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
-            _buildItemList('Granola', 'assets/images/granola.jpg'),
-            _buildItemList('Galleta Triturada', 'assets/images/galleta.jpg'),
-            _buildItemList('Coco', 'assets/images/coco.jpg'),
-          ],
+              const SizedBox(height: 20),
+              _buildSectionTitle('SABORES', context),
+              _buildItemList('Chocolate', '../assets/images/chocolate.jpg'),
+              _buildItemList('Vainilla', '../assets/images/vainilla.jpg'),
+              _buildItemList('Fresa', '../assets/images/fresa.jpg'),
+              const SizedBox(height: 20),
+              _buildSectionTitle('TOPPINGS', context),
+              _buildItemList('Granola', '../assets/images/granola.jpg'),
+              _buildItemList('Galleta Triturada', '../assets/images/galleta.jpg'),
+              _buildItemList('Coco', '../assets/images/coco.jpg'),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -132,26 +80,48 @@ class AdminPage extends StatelessWidget {
     );
   }
 
+  Widget _buildSectionTitle(String title, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.red,
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.add_circle, color: Colors.yellow),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _buildItemList(String title, String imagePath) {
     return Card(
-      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      margin: const EdgeInsets.symmetric(vertical: 8),
       child: ListTile(
-        leading:
-            Image.asset(imagePath, width: 50, height: 50, fit: BoxFit.cover),
-        title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+        leading: Image.asset(imagePath, width: 50, height: 50, fit: BoxFit.cover),
+        title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
             TextButton(
               onPressed: () {},
-              child: Text('EDITAR', style: TextStyle(color: Colors.red)),
+              child: const Text('EDITAR', style: TextStyle(color: Colors.red)),
             ),
             TextButton(
               onPressed: () {},
-              child: Text('ELIMINAR', style: TextStyle(color: Colors.red)),
+              child: const Text('ELIMINAR', style: TextStyle(color: Colors.red)),
             ),
             IconButton(
-              icon: Icon(Icons.favorite, color: Colors.red),
+              icon: const Icon(Icons.favorite, color: Colors.red),
               onPressed: () {},
             ),
           ],
--- flutter_app/lib/ui/registro/RegistroScreen.dart
+++ flutter_app/lib/ui/registro/RegistroScreen.dart
@@ -1,14 +1,101 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/rutas/constants.dart';
 
 class RegistroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sign In'),
+        title: const Text(
+          'Registro',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 195, 79, 79),
       ),
-      body: Center(
-        child: Text('Registros aqui'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                height: 150,
+              ),
+              const SizedBox(height: 24),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Nombre completo',
+                ),
+              ),
+              const SizedBox(height: 16),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Correo electrónico',
+                ),
+              ),
+              const SizedBox(height: 16),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Contraseña',
+                  suffixIcon: Icon(Icons.visibility_off),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirmar contraseña',
+                  suffixIcon: Icon(Icons.visibility_off),
+                ),
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar el botón de registro
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+                child: const Text(
+                  'Registrarse',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              RichText(
+                text: TextSpan(
+                  text: '¿Ya tienes cuenta? ',
+                  style: const TextStyle(color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Inicia sesión',
+                      style: const TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.of(context).pushReplacementNamed(LOGIN);
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
